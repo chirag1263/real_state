e3dd344dd680e86fdc6295dd2383f74f76e4d1d0
chirag <chiragverma2207@gmail.com>
2021-07-10 10:21:21 +0530

    change

--- site/app/Http/Controllers/FrontendController.php
+++ site/app/Http/Controllers/FrontendController.php
@@ -116,7 +116,12 @@ class FrontendController extends Controller {
           }
         }
 
-        $f_ids = Input::get('filters');
+        if(Input::has('filters')){
+
+		    	$f_ids = Input::get('filters');
+		    }else{
+		    	$f_ids = [];
+		    }
 		    if(sizeof($f_ids) > 0){
 		    	$project_ids = [];
 		    	foreach($f_ids as $fi){
