4541e51790465b2f8075bf724d332f7fc885c6dc
chirag <chiragverma2207@gmail.com>
2021-07-11 12:23:44 +0530

    cahge

--- site/app/User.php
+++ site/app/User.php
@@ -89,19 +89,19 @@ class User extends Authenticatable
             case 'wishlist-projects':
                 if(Auth::user()->priv == 1){
 
-                    $count = DB::table('wishlist')->select('id')->where('type',1)->count();
+                    $count = DB::table('enquiries')->select('id')->where('type',1)->count();
                 }else{
 
-                    $count = DB::table('wishlist')->select('id')->where('type',1)->where('user_id',Auth::id())->count();
+                    $count = DB::table('enquiries')->select('id')->where('type',1)->where('user_id',Auth::id())->count();
                 }
                 break;
             case 'wishlist-listing':
                 if(Auth::user()->priv == 1){
 
-                    $count = DB::table('wishlist')->select('id')->where('type',2)->count();
+                    $count = DB::table('enquiries')->select('id')->where('type',2)->count();
                 }else{
                     
-                    $count = DB::table('wishlist')->select('id')->where('type',2)->where('user_id',Auth::id())->count();
+                    $count = DB::table('enquiries')->select('id')->where('type',2)->where('user_id',Auth::id())->count();
                 }
                 break;
             case 'history-listing':
